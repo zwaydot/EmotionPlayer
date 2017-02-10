2927a2caa3803313387b8f38ea8ae113e0a4846d
zway <zwaydot@yeah.net>
2017-02-10 15:17:27 +0800

    no message

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(function(){
   $('.share').hide();
   $('.share').fadeIn(1500);
-  $('#wave').hide();
   $('.content').animate({bottom:'+=30px', opacity: 1} , 1000 );
   $('#playbutton').on('click', function() {
       var music = document.getElementById("music");
